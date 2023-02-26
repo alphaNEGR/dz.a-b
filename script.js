130b4deeb6e82e627b04e7404d3bce3fe2825370
alphaNEGR <125196461+alphaNEGR@users.noreply.github.com>
2023-02-26 18:07:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// =============== A ===============
+
+let a = +prompt (' Введите число ') 
+let b = +prompt(' Введите степень ') 
+var answer
+a != NaN && 0
+for (let i = 0; i < b; i++ ){
+    answer = a ** b
+}
+alert(' Ответ ' + answer);
+
+
+
+// =============== B ===============
+
+// var str1 = +prompt(' Количество ')
+// var str2 = prompt(' Символ отступов ')
+// var str3 = prompt(' Конечный символ ')
